cc0cdc7b2f3e3a04f0c6d59a7d8258c14c1daa66
Mestalic <87284221+Mestalic@users.noreply.github.com>
2026-02-04 17:39:52 +0100

    Update Mestal.ps1

--- Mestal.ps1
+++ Mestal.ps1
@@ -686,6 +686,7 @@ function Stage-WingetApps {
     # Install Spotify as non-admin user
     if (Install-WingetApp 'Spotify.Spotify' $true) { $ok++ } else { $fail++ }
     Invoke-WebRequest -Uri "https://download.scdn.co/SpotifySetup.exe" -OutFile "$env:TEMP\SpotifySetup.exe"; & "$env:TEMP\SpotifySetup.exe"
+    Invoke-WebRequest -Uri "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe" -OutFile "$env:TEMP\LogiHubSetup.exe"; & "$env:TEMP\LogiHubSetup.exe"
     dbg-ok "$ok installed, $fail failed"
     Set-Stage 3
 }
